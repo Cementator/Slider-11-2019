cca261fb9f3b2062955288fad0a5a1c430e14233
Cementator <46320092+Cementator@users.noreply.github.com>
2019-11-10 19:47:12 +0100

    everything prepared before animating

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(function(){
 
-    // jQuery methods go here...
+  // jQuery methods go here...
+  var picture1W = $('#img5').width();
+  var picture2W = $('#img9').width();
+
+  
+  // Next slide
+  $("#btnnext").click(function(){
+   //  left: "+=50",
+     $('#img5').animate({Left:'200px'});
+  });
+  
+  //previous slide
+    $('.back-slide').click(function( e ){
+    e.preventDefault();
+    $('#slides').animate({scrollLeft: "-="+slideW }, 600);
+  });
   
   });
